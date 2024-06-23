1281cf18a28ce8a51640dcefcef35a0784058fa4
marijajolovic <marija.jolovic.kg@gmail.com>
2024-06-24 01:16:25 +0200

    Added API for fetching but theres no backend

--- sandbox/main.py
+++ sandbox/main.py
@@ -1,4 +1,5 @@
 import requests
+import time
 
 def main():
 
@@ -16,6 +17,28 @@ def main():
     else:
         print(f"Failed to start AutoML task. Status code: {response.status_code}")
         print(response.text)
+
+    url_result = f"http://localhost:8000/automl/result/{task_id}"
+    while True:
+        response_result = requests.get(url_result)
+        if response_result.status_code == 200:
+            result_json = response_result.json()
+            status = result_json["status"]
+            if status == "SUCCESS":
+                model = result_json["result"]
+                print("AutoML task completed successfully.")
+                print("Fitted Model:", model)
+                break
+            elif status == "PENDING" or status == "PROCESSING":
+                print("AutoML task still processing...")
+            else:
+                print(f"AutoML task failed with status: {status}")
+                break
+        else:
+            print(f"Failed to fetch AutoML task result. Status code: {response_result.status_code}")
+            print(response_result.text)
+            break
+        time.sleep(5)  # Polling interval
 
 if __name__ == '__main__':
     main()
