1a7b2f13fd7cad2f6b5bd8d4c70cedefa6924b91
Drashko73 <radovandk@gmail.com>
2024-06-24 18:24:26 +0200

    chore: Refactor start_training.py and remove unnecessary code

--- src/metafox/tasks/start_training.py
+++ src/metafox/tasks/start_training.py
@@ -3,11 +3,8 @@ import pandas as pd
 from metafox.celery import app
 from metafox.ml_models.knn import KNNRegressor
 
-from time import sleep
-
 @app.task
 def start_automl_train(link_to_data: str, target: str):
-    print(f"Starting AutoML task with data: {link_to_data} and target: {target}")
     
     # Load the data
     data = pd.read_csv(link_to_data)
@@ -18,9 +15,6 @@ def start_automl_train(link_to_data: str, target: str):
     model = KNNRegressor()
     model.fit(X_train, y_train)
     
-    # store result using redis
     result = model.get_params()
     
-    # sleep(10)   # Simulate a long-running task (10 seconds
-    
-    return {"message": "AutoML task completed.", "model_params": model.get_params()}
+    return {"message": "AutoML task completed.", "model_params": result}
